8efca96c1354d4a936b58f40db364251e531c6dd
Bence Gombkötő <kucukasd@gmail.com>
2025-01-19 14:17:35 +0100

    init pygame

--- microbes_grid.py
+++ microbes_grid.py
@@ -1,13 +1,38 @@
 import pygame
 import pandas as pd
 
+WHITE = (255, 255, 255)
+
 
 class MicrobesGrid:
     def __init__(self, data_source):
         self.df = pd.read_excel(data_source)
+        self.width = 1280
+        self.height = 760
+        self.running = True
+
+        self.init_pygame()
+
+    def init_pygame(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Microbes Grid")
+
+    def event_handler(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
     def update(self):
-        pass
+        self.clock.tick(60)
+        self.screen.fill(WHITE)
 
     def main_loop(self):
-        pass
+        while self.running:
+            self.update()
+            self.event_handler()
+
+
+game = MicrobesGrid("microbes.xlsx")
+game.main_loop()
