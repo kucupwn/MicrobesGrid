563b1999534fefb961037679039b205089b490af
Bence Gombkötő <kucukasd@gmail.com>
2025-01-25 10:10:42 +0100

    add get custom shape property

--- game_dataset.py
+++ game_dataset.py
@@ -29,25 +29,47 @@ class GameDataset:
         # Automate name extract to list
         return df.apply(lambda row: f"{row['Genus']} {row['Species']}", axis=1).tolist()
 
+    def create_property_tuple(self, col, prop):
+        prop_df = self.df[self.df[col] == prop[0]]
+        prop_list = self.get_species_name_list(prop_df)
+        prop_result = (f"{col}:\n{prop[0]}", prop_list)
+
+        return prop_result
+
     def get_properties(self):
         for col in self.columns:
             if col == "Domain" or col == "Genus" or col == "Species":
                 continue
 
+            col_values = self.df.groupby(col, as_index=False).size()
+
             if col == "Shape":
-                pass
+                self.get_shape_property(col, col_values)
 
             if col == "GC Content":
-                pass
-
-            if col == "Biosafety Level":
-                pass
-
-            col_values = self.df.groupby(col, as_index=False).size()
+                self.get_gc_content_property(col, col_values)
 
             for _, prop in col_values.iterrows():
                 if prop["size"] > 2:
-                    prop_df = self.df[self.df[col] == prop[0]]
-                    prop_list = self.get_species_name_list(prop_df)
-                    prop_result = (f"{col}:\n{prop[0]}", prop_list)
+                    prop_result = self.create_property_tuple(col, prop)
                     self.properties.append(prop_result)
+
+    def get_shape_property(self, col, values):
+        for _, prop in values.iterrows():
+            if prop["size"] > 2:
+                if prop[0] == "Rod":
+                    prop_result = self.create_property_tuple(col, prop)
+                    self.properties.append(prop_result)
+                elif prop[0] in SPHERE_SHAPE:
+                    prop_df = self.df[self.df[col].isin(SPHERE_SHAPE)]
+                    prop_list = self.get_species_name_list(prop_df)
+                    prop_result = (f"{col}:\nSphere", prop_list)
+                    self.properties.append(prop_result)
+                elif prop[0] in OTHER_SHAPE:
+                    prop_df = self.df[self.df[col].isin(OTHER_SHAPE)]
+                    prop_list = self.get_species_name_list(prop_df)
+                    prop_result = (f"{col}:\nNOT Rod or Sphere", prop_list)
+                    self.properties.append(prop_result)
+
+    def get_gc_content_property(self):
+        pass
