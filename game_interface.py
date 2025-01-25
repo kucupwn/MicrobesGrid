b00c28c93fe0f40eb5efb2338e4678db7dc22bb2
Bence Gombkötő <kucukasd@gmail.com>
2025-01-25 19:04:45 +0100

    reorder interface methods

--- game_interface.py
+++ game_interface.py
@@ -33,6 +33,8 @@ class GameInterface:
         self.create_root_and_frame()
         self.get_labels_cells_and_game_cells()
 
+    # UI Setup
+
     def create_root_and_frame(self):
         self.root = tk.Tk()
         self.root.geometry(f"{self.width}x{self.height}")
@@ -40,145 +42,6 @@ class GameInterface:
 
         self.frame = tk.Frame(self.root)
         self.frame.pack(padx=40, pady=40)
-
-    def create_combobox(self):
-        # Create a new top-level window for the Combobox
-        combobox_window = tk.Toplevel(self.root)
-        combobox_window.title("Select Species")
-
-        combobox_width = 300
-        combobox_height = 60
-        position_left, position_top = self.window_placement_middle(
-            combobox_window, combobox_width, combobox_height
-        )
-        combobox_window.geometry(
-            f"{combobox_width}x{combobox_height}+{position_left}+{position_top}"
-        )
-
-        # Create the Combobox
-        combobox = AutocompleteCombobox(
-            combobox_window, completevalues=self.all_species
-        )
-        combobox.pack(padx=10, pady=10, fill=tk.X)
-
-        return {"window": combobox_window, "combobox": combobox}
-
-    def window_placement_middle(self, window, width, height):
-        screen_width = window.winfo_screenwidth()
-        screen_height = window.winfo_screenheight()
-        position_top = int(screen_height / 2 - height / 2)
-        position_left = int(screen_width / 2 - width / 2)
-
-        return (position_left, position_top)
-
-    def info_centre_combobox_events(self):
-        combobox = self.create_combobox()
-
-        def on_select(event):
-            selected_value = combobox["combobox"].get()
-            self.display_species_info(selected_value)
-            combobox["window"].grab_release()  # Release the lock
-            combobox["window"].destroy()  # Close the Toplevel window
-
-        def on_enter(event):
-            selected_value = combobox["combobox"].get()
-            if selected_value:
-                self.display_species_info(selected_value)
-                combobox["window"].grab_release()
-                combobox["window"].destroy()
-
-        combobox["combobox"].bind("<<ComboboxSelected>>", on_select)
-        combobox["combobox"].bind("<Return>", on_enter)
-
-        combobox["combobox"].focus()
-
-    def display_species_info(self, name):
-        # Split name into Genus and Species
-        name_cols = name.split(" ")
-
-        # Filter the DataFrame based on Genus and Species
-        name_row = self.df[
-            (self.df["Genus"] == name_cols[0]) & (self.df["Species"] == name_cols[1])
-        ]
-
-        # Create a new top-level window for the table view
-        info_window = tk.Toplevel(self.root)
-        info_window.title(f"Details for {name}")
-
-        # Center the Toplevel window on the screen
-        window_width = 350
-        window_height = 680
-        position_left, position_top = self.window_placement_middle(
-            info_window, window_width, window_height
-        )
-        info_window.geometry(
-            f"{window_width}x{window_height}+{position_left}+{position_top}"
-        )
-
-        # Add key-value pairs in the format "Column Name: Value"
-        for col in self.df.columns:
-            value = name_row.iloc[0][col]  # Get the value for the current column
-            tk.Label(
-                info_window,
-                text=f"{col}: {value}",
-                font=("Arial", 12),
-                anchor="w",
-                justify="left",
-                padx=10,
-                pady=5,
-            ).pack(anchor="w")
-
-    def input_combobox_events(self, button):
-        if button.cget("text") != UNKNOWN:
-            return
-
-        combobox = self.create_combobox()
-
-        def on_select(event):
-            selected_value = combobox["combobox"].get()
-            self.check_user_input(selected_value, button)  # Update the button text
-            combobox["window"].destroy()  # Close the Toplevel window
-
-        def on_enter(event):
-            selected_value = combobox["combobox"].get()
-            if selected_value:
-                self.check_user_input(selected_value, button)
-                combobox["window"].destroy()
-
-        combobox["combobox"].bind("<<ComboboxSelected>>", on_select)
-        combobox["combobox"].bind("<Return>", on_enter)
-
-        combobox["combobox"].focus()
-
-    def check_user_input(self, selected_value, button):
-        # Calculate the button's row and column index
-        button_index = self.game_fields.index(button)
-        row_index = button_index // len(self.col_props)
-        col_index = button_index % len(self.col_props)
-
-        # Get the intersection related to this row and column combination
-        intersection = self.intersections[row_index][col_index]
-
-        # Check if the selected value exists in the intersection for this position
-        if selected_value in intersection:
-            line_break_name = selected_value.replace(" ", "\n")
-            button.config(text=line_break_name)
-        else:
-            button.config(text=UNKNOWN)
-            button.config(bg="red")
-            self.reset_button_bg_delayed(button)
-
-    def reset_button_bg_delayed(self, button):
-        button.after(1000, lambda: button.config(bg="lightgray"))
-
-    def reset_ui(self, cols, rows, game_fields):
-        for widget in self.frame.winfo_children():
-            widget.destroy()
-
-        self.col_props = cols
-        self.row_props = rows
-        self.game_fields = game_fields
-        self.get_labels_cells_and_game_cells()
 
     def get_labels_cells_and_game_cells(self):
         # Set up gamefield
@@ -258,3 +121,146 @@ class GameInterface:
             command=self.info_centre_combobox_events,
         )
         info_button.grid(row=4, column=0, padx=10, pady=10)
+
+    def reset_ui(self, cols, rows, game_fields):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+
+        self.col_props = cols
+        self.row_props = rows
+        self.game_fields = game_fields
+        self.get_labels_cells_and_game_cells()
+
+    # Event handling
+
+    def create_combobox(self):
+        # Create a new top-level window for the Combobox
+        combobox_window = tk.Toplevel(self.root)
+        combobox_window.title("Select Species")
+
+        combobox_width = 300
+        combobox_height = 60
+        position_left, position_top = self.window_placement_middle(
+            combobox_window, combobox_width, combobox_height
+        )
+        combobox_window.geometry(
+            f"{combobox_width}x{combobox_height}+{position_left}+{position_top}"
+        )
+
+        # Create the Combobox
+        combobox = AutocompleteCombobox(
+            combobox_window, completevalues=self.all_species
+        )
+        combobox.pack(padx=10, pady=10, fill=tk.X)
+
+        return {"window": combobox_window, "combobox": combobox}
+
+    def input_combobox_events(self, button):
+        if button.cget("text") != UNKNOWN:
+            return
+
+        combobox = self.create_combobox()
+
+        def on_select(event):
+            selected_value = combobox["combobox"].get()
+            self.check_user_input(selected_value, button)  # Update the button text
+            combobox["window"].destroy()  # Close the Toplevel window
+
+        def on_enter(event):
+            selected_value = combobox["combobox"].get()
+            if selected_value:
+                self.check_user_input(selected_value, button)
+                combobox["window"].destroy()
+
+        combobox["combobox"].bind("<<ComboboxSelected>>", on_select)
+        combobox["combobox"].bind("<Return>", on_enter)
+
+        combobox["combobox"].focus()
+
+    def check_user_input(self, selected_value, button):
+        # Calculate the button's row and column index
+        button_index = self.game_fields.index(button)
+        row_index = button_index // len(self.col_props)
+        col_index = button_index % len(self.col_props)
+
+        # Get the intersection related to this row and column combination
+        intersection = self.intersections[row_index][col_index]
+
+        # Check if the selected value exists in the intersection for this position
+        if selected_value in intersection:
+            line_break_name = selected_value.replace(" ", "\n")
+            button.config(text=line_break_name)
+        else:
+            button.config(text=UNKNOWN)
+            button.config(bg="red")
+            self.reset_button_bg_delayed(button)
+
+    def reset_button_bg_delayed(self, button):
+        button.after(1000, lambda: button.config(bg="lightgray"))
+
+    def info_centre_combobox_events(self):
+        combobox = self.create_combobox()
+
+        def on_select(event):
+            selected_value = combobox["combobox"].get()
+            self.display_species_info(selected_value)
+            combobox["window"].grab_release()  # Release the lock
+            combobox["window"].destroy()  # Close the Toplevel window
+
+        def on_enter(event):
+            selected_value = combobox["combobox"].get()
+            if selected_value:
+                self.display_species_info(selected_value)
+                combobox["window"].grab_release()
+                combobox["window"].destroy()
+
+        combobox["combobox"].bind("<<ComboboxSelected>>", on_select)
+        combobox["combobox"].bind("<Return>", on_enter)
+
+        combobox["combobox"].focus()
+
+    def display_species_info(self, name):
+        # Split name into Genus and Species
+        name_cols = name.split(" ")
+
+        # Filter the DataFrame based on Genus and Species
+        name_row = self.df[
+            (self.df["Genus"] == name_cols[0]) & (self.df["Species"] == name_cols[1])
+        ]
+
+        # Create a new top-level window for the table view
+        info_window = tk.Toplevel(self.root)
+        info_window.title(f"Details for {name}")
+
+        # Center the Toplevel window on the screen
+        window_width = 350
+        window_height = 680
+        position_left, position_top = self.window_placement_middle(
+            info_window, window_width, window_height
+        )
+        info_window.geometry(
+            f"{window_width}x{window_height}+{position_left}+{position_top}"
+        )
+
+        # Add key-value pairs in the format "Column Name: Value"
+        for col in self.df.columns:
+            value = name_row.iloc[0][col]  # Get the value for the current column
+            tk.Label(
+                info_window,
+                text=f"{col}: {value}",
+                font=("Arial", 12),
+                anchor="w",
+                justify="left",
+                padx=10,
+                pady=5,
+            ).pack(anchor="w")
+
+    # Util
+
+    def window_placement_middle(self, window, width, height):
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        position_top = int(screen_height / 2 - height / 2)
+        position_left = int(screen_width / 2 - width / 2)
+
+        return (position_left, position_top)
