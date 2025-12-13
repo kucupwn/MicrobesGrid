51a777fa31b5051329da8dc7ef21151c372e44f8
Bence Gombkötő <kucukasd@gmail.com>
2025-12-13 13:28:19 +0100

    update get_xlsx_file

--- data/data_utils.py
+++ data/data_utils.py
@@ -2,13 +2,14 @@ import os
 import sys
 import pandas as pd
 
+
 def get_xlsx_file():
-    if getattr(sys, "frozen", False):
-        # Running as a bundled .exe
-        return os.path.join(sys._MEIPASS, "microbes.xlsx")
-    else:
-        # Running as a normal Python script
-        return os.path.join(os.path.dirname(__file__), "microbes.xlsx")
+    base_path = (
+        sys._MEIPASS
+        if getattr(sys, "frozen", False)
+        else os.path.dirname(os.path.abspath(__file__))
+    )
+    return os.path.join(base_path, "data", "microbes.xlsx")
 
 
 def load_dataset():
